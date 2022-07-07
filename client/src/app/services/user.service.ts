b562393e2c4694da056d13f10a096c18668a0bf4
rcq23.2012@gmail.com <rcq23.2012@gmail.com>
2022-07-07 12:59:55 +0000

    File generated by Skaffolder - user: rcq23.2012@gmail.com

---
+++ client/src/app/services/user.service.ts
@@ -0,0 +1,17 @@
+// BASE SERVICE
+import { UserBaseService } from './base/user.base.service';
+
+
+// start documentation
+/**
+ * Custom APIs
+ *
+ */
+// end documentation
+
+/**
+ * YOU CAN OVERRIDE HERE UserBaseService
+ */
+export class UserService extends UserBaseService {
+
+}
